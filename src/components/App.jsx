5c889c878fddff5856be7c13e165a6d060bdc23d
Luke Vandekieft <vandekie@gmail.com>
2018-12-27 16:19:10 -0800

    Set up working React environment.

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+//styles
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Is this thing on?
+      </div>
+    );
+  }
+}
+
+export default App;
