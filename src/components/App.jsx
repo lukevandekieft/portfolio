aed663e2a2b62b8a887dbb9366ef1a8006a4f294
Luke Vandekieft <vandekie@gmail.com>
2018-12-27 17:17:20 -0800

    Set up basic hyperlinks.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,12 @@ class App extends Component {
   render() {
     return (
       <div>
-        Is this thing on?
+      Luke's Site Portfolio
+        <a href="https://www.halfbloodquince.com">Half-Blood quince</a>
+        <a href="https://travel-peru.firebaseapp.com">Travel Peru</a>
+        <a href="https://thinkgeek-clone.firebaseapp.com">ThinkGeek Site Clone</a>
+        <a href="https://crestfallen-game.firebaseapp.com">Crestfallen</a>
+        <a href="https://travel-peru.firebaseapp.com">Travel Peru</a>
       </div>
     );
   }
