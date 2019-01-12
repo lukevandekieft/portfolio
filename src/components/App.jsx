52f4b23f4ef8682665595c2589b953a7896f57f6
Luke Vandekieft <vandekie@gmail.com>
2019-01-12 00:07:38 -0800

    Convert App.jsx labels from HTML to React.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,11 +11,11 @@ import '../assets/sass/main.scss';
 class App extends Component {
   render() {
     return (
-      <body class="is-preload">
+      <div className="is-preload">
 			   <div id="page-wrapper">
 
 					<section id="banner">
-						<div class="inner">
+						<div className="inner">
 							<h2>This is Solid State</h2>
 							<p>Another free + fully responsive site template by <a href="http://html5up.net">HTML5 UP</a></p>
 						</div>
@@ -23,71 +23,45 @@ class App extends Component {
 
 					<section id="wrapper">
 
-						<section id="one" class="wrapper spotlight style1">
-							<div class="inner">
-								<a href="https://www.halfbloodquince.com" class="image"><img src={siteQuince} alt="Half-Blood Quince Site" /></a>
-								<div class="content">
-									<h2 class="major">Half-Blood Quince</h2>
+						<section id="one" className="wrapper spotlight style1">
+							<div className="inner">
+								<a href="https://www.halfbloodquince.com" className="image"><img src={siteQuince} alt="Half-Blood Quince Site" /></a>
+								<div className="content">
+									<h2 className="major">Half-Blood Quince</h2>
 									<p>Eat it!</p>
-									<a href="https://www.halfbloodquince.com" class="special">Learn more</a>
+									<a href="https://www.halfbloodquince.com" className="special">Learn more</a>
 								</div>
 							</div>
 						</section>
 
-						<section id="two" class="wrapper alt spotlight style2">
-							<div class="inner">
-								<a href="https://thinkgeek-clone.firebaseapp.com" class="image"><img src={siteGeek} alt="ThinkGeek Clone Site" /></a>
-								<div class="content">
-									<h2 class="major">ThinkGeek Site Clone</h2>
+						<section id="two" className="wrapper alt spotlight style2">
+							<div className="inner">
+								<a href="https://thinkgeek-clone.firebaseapp.com" className="image"><img src={siteGeek} alt="ThinkGeek Clone Site" /></a>
+								<div className="content">
+									<h2 className="major">ThinkGeek Site Clone</h2>
 									<p>Look how good this is!</p>
-									<a href="https://thinkgeek-clone.firebaseapp.com" class="special">Learn more</a>
+									<a href="https://thinkgeek-clone.firebaseapp.com" className="special">Learn more</a>
 								</div>
 							</div>
 						</section>
 
-						<section id="three" class="wrapper spotlight style3">
-							<div class="inner">
-								<a href="https://travel-peru.firebaseapp.com" class="image"><img src={sitePeru} alt="Travel Peru Site" /></a>
-								<div class="content">
-									<h2 class="major">Travel Peru</h2>
+						<section id="three" className="wrapper spotlight style3">
+							<div className="inner">
+								<a href="https://travel-peru.firebaseapp.com" className="image"><img src={sitePeru} alt="Travel Peru Site" /></a>
+								<div className="content">
+									<h2 className="major">Travel Peru</h2>
 									<p>Work in Practice</p>
-									<a href="https://travel-peru.firebaseapp.com" class="special">Learn more</a>
+									<a href="https://travel-peru.firebaseapp.com" className="special">Learn more</a>
 								</div>
 							</div>
 						</section>
 
-						<section id="four" class="wrapper alt style1">
-							<div class="inner">
-								<h2 class="major">Crestfallen - Keep?</h2>
+						<section id="four" className="wrapper alt style1">
+							<div className="inner">
+								<h2 className="major">Crestfallen - Keep?</h2>
 								<p>Don't forget about this link: <a href="https://crestfallen-game.firebaseapp.com">Crestfallen</a></p>
-								<section class="features">
-									<article>
-										<a href="#" class="image"><img src="images/pic04.jpg" alt="" /></a>
-										<h3 class="major">Sed feugiat lorem</h3>
-										<p>Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id nulla dignissim dapibus ultrices.</p>
-										<a href="#" class="special">Learn more</a>
-									</article>
-									<article>
-										<a href="#" class="image"><img src="images/pic05.jpg" alt="" /></a>
-										<h3 class="major">Nisl placerat</h3>
-										<p>Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id nulla dignissim dapibus ultrices.</p>
-										<a href="#" class="special">Learn more</a>
-									</article>
-									<article>
-										<a href="#" class="image"><img src="images/pic06.jpg" alt="" /></a>
-										<h3 class="major">Ante fermentum</h3>
-										<p>Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id nulla dignissim dapibus ultrices.</p>
-										<a href="#" class="special">Learn more</a>
-									</article>
-									<article>
-										<a href="#" class="image"><img src="images/pic07.jpg" alt="" /></a>
-										<h3 class="major">Fusce consequat</h3>
-										<p>Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id nulla dignissim dapibus ultrices.</p>
-										<a href="#" class="special">Learn more</a>
-									</article>
-								</section>
-								<ul class="actions">
-									<li><a href="#" class="button">Browse All</a></li>
+								<ul className="actions">
+									<li><a href="#" className="button">Browse All</a></li>
 								</ul>
 							</div>
 						</section>
@@ -95,41 +69,41 @@ class App extends Component {
 				</section>
 
 				<section id="footer">
-					<div class="inner">
-						<h2 class="major">Get in touch</h2>
+					<div className="inner">
+						<h2 className="major">Get in touch</h2>
 						<p>Cras mattis ante fermentum, malesuada neque vitae, eleifend erat. Phasellus non pulvinar erat. Fusce tincidunt, nisl eget mattis egestas, purus ipsum consequat orci, sit amet lobortis lorem lacus in tellus. Sed ac elementum arcu. Quisque placerat auctor laoreet.</p>
 						<form method="post" action="#">
-							<div class="fields">
-								<div class="field">
+							<div className="fields">
+								<div className="field">
 									<label for="name">Name</label>
 									<input type="text" name="name" id="name" />
 								</div>
-								<div class="field">
+								<div className="field">
 									<label for="email">Email</label>
 									<input type="email" name="email" id="email" />
 								</div>
-								<div class="field">
+								<div className="field">
 									<label for="message">Message</label>
 									<textarea name="message" id="message" rows="4"></textarea>
 								</div>
 							</div>
-							<ul class="actions">
+							<ul className="actions">
 								<li><input type="submit" value="Send Message" /></li>
 							</ul>
 						</form>
-						<ul class="contact">
-							<li class="fa-home">
+						<ul className="contact">
+							<li className="fa-home">
 								Untitled Inc<br />
 								1234 Somewhere Road Suite #2894<br />
 								Nashville, TN 00000-0000
 							</li>
-							<li class="fa-phone">(000) 000-0000</li>
-							<li class="fa-envelope"><a href="#">information@untitled.tld</a></li>
-							<li class="fa-twitter"><a href="#">twitter.com/untitled-tld</a></li>
-							<li class="fa-facebook"><a href="#">facebook.com/untitled-tld</a></li>
-							<li class="fa-instagram"><a href="#">instagram.com/untitled-tld</a></li>
+							<li className="fa-phone">(000) 000-0000</li>
+							<li className="fa-envelope"><a href="#">information@untitled.tld</a></li>
+							<li className="fa-twitter"><a href="#">twitter.com/untitled-tld</a></li>
+							<li className="fa-facebook"><a href="#">facebook.com/untitled-tld</a></li>
+							<li className="fa-instagram"><a href="#">instagram.com/untitled-tld</a></li>
 						</ul>
-						<ul class="copyright">
+						<ul className="copyright">
 							<li>&copy; Untitled Inc. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
 						</ul>
 					</div>
@@ -141,7 +115,7 @@ class App extends Component {
 			<script src="assets/js/breakpoints.min.js"></script>
 			<script src="assets/js/util.js"></script>
 			<script src="assets/js/main.js"></script>
-	</body>
+	</div>
     );
   }
 }
